f30ce461b11af82eb6876410660c69d2a61146d2
kiran <kirangummadi1985@gmail.com>
2022-06-16 00:47:17 +0530

    rabbitmq

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-devopsb64"
-    key    = "vpc/dev/${var.COMPONENT}/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#data "terraform_remote_state" "vpc" {
+#  backend = "s3"
+#  config = {
+#    bucket = "terraform-devopsb64"
+#    key    = "vpc/dev/${var.COMPONENT}/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
 
 data "aws_secretsmanager_secret" "secret" {
   name = "roboshop"
