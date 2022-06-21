54c44946751fd10536e4300beefeb81a1e87e369
kiran <kirangummadi1985@gmail.com>
2022-06-21 23:55:41 +0530

    Infra-setup

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_db_instance" "main" {
   parameter_group_name = aws_db_parameter_group.main.name
   skip_final_snapshot  = var.SKIP_FINAL_SNAPSHOT
   db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
 
 
